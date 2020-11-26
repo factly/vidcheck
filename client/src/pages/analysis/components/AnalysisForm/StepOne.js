fcdafe9d9601cdd1d9bb4c1058c3aacccd74e208
shreeharsha-factly <harsha@factly.in>
2020-11-26 13:26:25 +0530

    removed save button in summary form and can sumbit form by clicking next

--- client/src/pages/analysis/components/AnalysisForm/StepOne.js
+++ client/src/pages/analysis/components/AnalysisForm/StepOne.js
@@ -1,14 +1,52 @@
-import { Button } from "antd";
+import { Button, Input } from "antd";
 import React from "react";
-import SummaryForm from "../SummaryForm";
 
 function StepOne({ current, summary, setSummary, setCurrent }) {
   return (
     <div style={current === 0 ? { display: "block" } : { display: "none" }}>
-      <SummaryForm summary={summary} setSummary={setSummary} />
-
+      <div style={{ marginTop: 16, width: "70%" }}>
+        <Input
+          addonBefore={"Title"}
+          placeholder={"Enter title"}
+          defaultValue={summary.title ? summary.title : ""}
+          onChange={(e) => setSummary({ ...summary, title: e.target.value })}
+        />
+      </div>
+      <div style={{ marginTop: 16, width: "70%", display: "flex" }}>
+        {/* <div className="ant-input-group-addon">Summary</div> */}
+        <div
+          style={{
+            border: "1px solid #d9d9d9",
+            padding: "0 11px",
+            fontWeight: "normal",
+            fontSize: 14,
+            textAlign: "center",
+            backgroundColor: "#fafafa",
+          }}
+        >
+          <p>Summary</p>
+        </div>
+        <Input.TextArea
+          addonBefore={"Summary"}
+          defaultValue={summary.summary ? summary.summary : ""}
+          onChange={(e) => setSummary({ ...summary, summary: e.target.value })}
+        />
+      </div>
+      <div style={{ marginTop: 16, width: "70%" }}>
+        <Input
+          addonBefore={"URL"}
+          placeholder={"Paste url here"}
+          defaultValue={summary.url ? summary.url : ""}
+          onChange={(e) => setSummary({ ...summary, url: e.target.value })}
+        />
+      </div>
       <div style={{ display: "flex", "justify-content": "flex-end" }}>
-        <Button type="primary" onClick={() => setCurrent(1)}>
+        <Button
+          type="primary"
+          onClick={() => {
+            setCurrent(1);
+          }}
+        >
           Next
         </Button>
       </div>
