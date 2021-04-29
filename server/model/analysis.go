4b3f8d8c9aaf3ed3ba165eb209154b175b9f913c
shreeharsha-factly <harsha@factly.in>
2021-04-29 18:48:57 +0530

    change claim, fact from json blocks to string

--- server/model/analysis.go
+++ server/model/analysis.go
@@ -13,14 +13,13 @@ type Analysis struct {
 	Video           *Video         `json:"video"`
 	RatingID        uint           `gorm:"column:rating_id" json:"rating_id"`
 	Rating          *Rating        `json:"rating"`
-	HTMLFact        string         `gorm:"column:html_fact" json:"html_fact,omitempty"`
 	HTMLDescription string         `gorm:"column:html_description" json:"html_description,omitempty"`
-	Claim           postgres.Jsonb `gorm:"column:claim" json:"claim"  swaggertype:"primitive,string"`
+	Claim           string         `gorm:"column:claim" json:"claim"`
 	Description     postgres.Jsonb `gorm:"column:description" json:"description" swaggertype:"primitive,string"`
-	ClaimDate       time.Time      `gorm:"column:claim_date" json:"claim_date" sql:"DEFAULT:NULL"`
-	CheckedDate     time.Time      `gorm:"column:checked_date" json:"checked_date" sql:"DEFAULT:NULL"`
+	ClaimDate       *time.Time     `gorm:"column:claim_date" json:"claim_date" sql:"DEFAULT:NULL"`
+	CheckedDate     *time.Time     `gorm:"column:checked_date" json:"checked_date" sql:"DEFAULT:NULL"`
 	IsClaim         bool           `gorm:"column:is_claim" json:"is_claim"`
-	Fact            postgres.Jsonb `gorm:"column:fact" json:"fact"  swaggertype:"primitive,string"`
+	Fact            string         `gorm:"column:fact" json:"fact"  swaggertype:"primitive,string"`
 	ClaimantID      uint           `gorm:"column:claimant_id" json:"claimant_id"`
 	Claimant        *Claimant      `json:"claimant"`
 	ReviewSources   postgres.Jsonb `gorm:"column:review_sources" json:"review_sources" swaggertype:"primitive,string"`
