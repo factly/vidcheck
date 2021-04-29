31d7fa38c4533c3e24c6bcf62b56e458926a2b23
shreeharsha-factly <harsha@factly.in>
2021-04-29 19:46:13 +0530

    add set analysis action

--- client/src/pages/videos/index.js
+++ client/src/pages/videos/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Button, Space } from "antd";
 import { Link } from "react-router-dom";
 import VideoList from "./components/VideoList";
+import { useDispatch } from "react-redux";
+import { resetClaim } from "../../actions/analysis";
 
 function Dashboard() {
+  const dispatch = useDispatch();
   return (
     <Space direction="vertical">
       <Link to="/analysis/create">
-        <Button>Create New</Button>
+        <Button onClick={() => dispatch(resetClaim())}>Create New</Button>
       </Link>
       <VideoList />
     </Space>
