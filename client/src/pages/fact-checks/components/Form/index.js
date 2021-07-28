87fed12e631e41400c4bb11282c8137e011ab2c1
shreeharsha-factly <harsha@factly.in>
2021-07-28 17:24:05 +0530

    add prettier changes

--- client/src/pages/fact-checks/components/Form/index.js
+++ client/src/pages/fact-checks/components/Form/index.js
@@ -184,8 +184,8 @@ function Analysis({ onSubmit }) {
           values.status = status;
           values.status === "publish"
             ? (values.published_date = values.published_date
-              ? moment(values.published_date).format("YYYY-MM-DDTHH:mm:ssZ")
-              : moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"))
+                ? moment(values.published_date).format("YYYY-MM-DDTHH:mm:ssZ")
+                : moment(Date.now()).format("YYYY-MM-DDTHH:mm:ssZ"))
             : (values.published_date = null);
           onSubmit({
             video: {
