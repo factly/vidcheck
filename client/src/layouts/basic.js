711bbea1f206d15e635a893f6141abecbc1e8941
shreeharsha-factly <harsha@factly.in>
2021-09-01 18:45:46 +0530

    fix: update children layout

--- client/src/layouts/basic.js
+++ client/src/layouts/basic.js
@@ -9,84 +9,88 @@ import "./basic.css";
 import { getSuperOrganisation } from "../actions/admin";
 
 function BasicLayout(props) {
-  const { Footer, Content } = Layout;
-  const { children } = props;
-  const dispatch = useDispatch();
+	const { Footer, Content } = Layout;
+	const { children } = props;
+	const dispatch = useDispatch();
 
-  const { permission, orgs, loading, selected, applications } = useSelector(
-    (state) => {
-      const { selected, orgs, loading } = state.spaces;
+	const { permission, orgs, loading, selected, applications } = useSelector(
+		(state) => {
+			const { selected, orgs, loading } = state.spaces;
 
-      if (selected > 0) {
-        const space = state.spaces.details[selected];
+			if (selected > 0) {
+				const space = state.spaces.details[selected];
 
-        const applications =
-          orgs.find((org) => org.spaces.includes(space.id))?.applications || [];
+				const applications =
+					orgs.find((org) => org.spaces.includes(space.id))?.applications || [];
 
-        return {
-          applications: applications,
-          permission: space.permissions || [],
-          orgs: orgs,
-          loading: loading,
-          selected: selected,
-        };
-      }
-      return {
-        orgs: orgs,
-        loading: loading,
-        permission: [],
-        selected: selected,
-        applications: [],
-      };
-    }
-  );
+				return {
+					applications: applications,
+					permission: space.permissions || [],
+					orgs: orgs,
+					loading: loading,
+					selected: selected,
+				};
+			}
+			return {
+				orgs: orgs,
+				loading: loading,
+				permission: [],
+				selected: selected,
+				applications: [],
+			};
+		}
+	);
 
-  const { type, message, description, time } = useSelector((state) => {
-    return { ...state.notifications };
-  });
+	const { type, message, description, time } = useSelector((state) => {
+		return { ...state.notifications };
+	});
 
-  const superOrg = useSelector(({ admin }) => {
-    return admin.organisation;
-  });
+	const superOrg = useSelector(({ admin }) => {
+		return admin.organisation;
+	});
 
-  React.useEffect(() => {
-    dispatch(getSpaces()).then((org) => {
-      if (org && org.length > 0) dispatch(getSuperOrganisation(org[0].id));
-    });
-  }, [dispatch, selected]);
+	React.useEffect(() => {
+		dispatch(getSpaces()).then((org) => {
+			if (org && org.length > 0) dispatch(getSuperOrganisation(org[0].id));
+		});
+	}, [dispatch, selected]);
 
-  React.useEffect(() => {
-    if (type && message && description && selected !== 0) {
-      notification[type]({
-        message: message,
-        description: description,
-      });
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [time, description]);
+	React.useEffect(() => {
+		if (type && message && description && selected !== 0) {
+			notification[type]({
+				message: message,
+				description: description,
+			});
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [time, description]);
 
-  return (
-    <Layout hasSider={true}>
-      <Sidebar
-        permission={permission}
-        orgs={orgs}
-        loading={loading}
-        superOrg={superOrg}
-      />
-      <Layout>
-        <Header applications={applications} />
-        <Content className="layout-content">
-          <Card key={selected.toString()} className="wrap-children-content">
-            {children}
-          </Card>
-        </Content>
-        {/* <Footer style={{ textAlign: "center" }}>
+	return (
+		<Layout hasSider={true}>
+			<Sidebar
+				permission={permission}
+				orgs={orgs}
+				loading={loading}
+				superOrg={superOrg}
+			/>
+			<Layout>
+				<Header applications={applications} />
+				<Content className="layout-content">
+					<Card
+						key={selected.toString()}
+						className="wrap-children-content"
+						bodyStyle={{ display: "flex", justifyContent: "center" }}
+					>
+						{children}
+					</Card>
+				</Content>
+				{/* <Footer style={{ textAlign: "center" }}>
           {" "}
           ©2014-2020 Factly Media & Research
         </Footer> */}
-      </Layout>
-    </Layout>
-  );
+			</Layout>
+		</Layout>
+	);
 }
 
 export default withRouter(BasicLayout);
